a86a3f6aed8ce070c831e9a91c0165b07ed9223b
n1claren <nikolay1892@abv.bg>
2025-01-03 00:05:29 +0200

    added dynamic creation of figures

--- 16.Polymorphism/Program.cs
+++ 16.Polymorphism/Program.cs
@@ -1,13 +1,67 @@
 ﻿using ShapeAreaCalculator;
 
-List<Shape> shapes = new List<Shape>
-{
-    new Circle(5),
-    new Rectangle(4, 6),
-    new Triangle(5, 8)
-};
+List<Shape> shapes = new List<Shape>();
 
-foreach (Shape shape in shapes)
+string command = String.Empty;
+
+while (true)
+{
+    Console.WriteLine("Enter the type of figure you want to create. (circle, rectangle, rhombus, square or triangle)");
+    Console.WriteLine("If you dont want to create any more figures, write 'calculate'");
+
+    command = Console.ReadLine().ToLower();
+
+    if (command == "calculate")
+    {
+        break;
+    }
+
+    switch (command.ToLower())
+    {
+        case "circle":
+            double radius = double.Parse(Console.ReadLine());
+            Circle circle = new Circle(radius);
+            shapes.Add(circle);
+            break;
+
+        case "rectangle":
+            double sideA = double.Parse(Console.ReadLine());
+            double sideB = double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(sideA, sideB);
+            shapes.Add(rectangle);
+            break;
+
+        case "rhombus":
+            double firstDiagonal = double.Parse(Console.ReadLine());
+            double secondDiagonal = double.Parse(Console.ReadLine());
+            double side = double.Parse(Console.ReadLine());
+            Rhombus rhombus = new Rhombus(firstDiagonal, secondDiagonal, side);
+            shapes.Add(rhombus);
+            break;
+
+        case "square":
+            double squareSide = double.Parse(Console.ReadLine());
+            Square square = new Square(squareSide);
+            shapes.Add(square);
+            break;
+
+        case "triangle":
+            double @base = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double leftSide = double.Parse(Console.ReadLine());
+            double rightSide = double.Parse(Console.ReadLine());
+            Triangle triangle = new Triangle(@base, height, leftSide, rightSide);
+            shapes.Add(triangle);
+            break;
+
+        default:
+            break;
+    }
+}
+
+foreach (var shape in shapes)
 {
     Console.WriteLine($"The area of the {shape.GetType().Name} is {shape.GetArea():F2}");
+    Console.WriteLine($"The perimeter of the {shape.GetType().Name} is {shape.GetPerimeter():F2}");
+    Console.WriteLine();
 }
