58049cf0a2565b50717f6bc3c4072e6561ee7ea6
n1claren <nikolay1892@abv.bg>
2025-01-02 22:59:33 +0200

    added two new figures

---
+++ 16.Polymorphism/Rhombus.cs
@@ -0,0 +1,20 @@
+﻿namespace ShapeAreaCalculator
+{
+    public class Rhombus : Shape
+    {
+        public double FirstDiagonal { get; set; }
+
+        public double SecondDiagonal { get; set; }
+
+        public Rhombus(double d1, double d2)
+        {
+            FirstDiagonal = d1;
+            SecondDiagonal = d2;
+        }
+
+        public override double GetArea()
+        {
+            return 0.5 * FirstDiagonal * SecondDiagonal;
+        }
+    }
+}
---
+++ 16.Polymorphism/Square.cs
@@ -0,0 +1,17 @@
+﻿namespace ShapeAreaCalculator
+{
+    public class Square : Shape
+    {
+        public double Side { get; set; }
+
+        public Square(double side)
+        {
+            Side = side;
+        }
+
+        public override double GetArea()
+        {
+            return Side * Side;
+        }
+    }
+}
