f98d1e2db5ab3c767b36dba9bec42fc52751469a
n1claren <nikolay1892@abv.bg>
2025-01-07 22:41:29 +0200

    added saving / loading shapes to / from JSON and improved code quality

--- 16.Polymorphism/Program.cs
+++ 16.Polymorphism/Program.cs
@@ -1,19 +1,21 @@
-﻿using ShapeAreaCalculator;
+﻿using System.Text.Json;
+using ShapeAreaCalculator;
 
-List<Shape> shapes = new List<Shape>();
+List<Shape> shapes = LoadShapes();
 
-string command = String.Empty;
+string command = string.Empty;
 
 while (true)
 {
-    Console.WriteLine("Enter the type of figure you want to create. (circle, rectangle, rhombus, square or triangle)");
-    Console.WriteLine("If you dont want to create any more figures, write 'calculate'");
+    Console.WriteLine("Enter the type of figure you want to create. (circle, rectangle, rhombus, square, or triangle)");
+    Console.WriteLine("If you don't want to create any more figures, write 'calculate'");
     Console.WriteLine("If you want to remove a figure, write 'remove'");
 
     command = Console.ReadLine().ToLower();
 
     if (command == "calculate")
     {
+        SaveShapes(shapes);
         break;
     }
 
@@ -26,229 +28,63 @@ while (true)
         }
 
         Console.WriteLine("Please enter the index of the figure you want to remove:");
-
         for (int i = 0; i < shapes.Count; i++)
         {
             Console.WriteLine($"Index {i} - figure: {shapes[i].GetType().Name} with an area of {shapes[i].GetArea():F2}");
         }
 
-        int index = -1;
-
-        while (true)
+        if (int.TryParse(Console.ReadLine(), out int index) && index >= 0 && index < shapes.Count)
         {
-            string input = Console.ReadLine();
-
-            if (int.TryParse(input, out index) && index >= 0 && index < shapes.Count)
-            {
-                break;
-            }
-            else
-            {
-                Console.WriteLine("You entered an invalid index. Try again.");
-            }
+            string nameOfRemovedFigure = shapes[index].GetType().Name;
+            shapes.RemoveAt(index);
+            Console.WriteLine($"Removed the {nameOfRemovedFigure} at index {index}");
         }
-
-        string nameOfRemovedFigure = shapes[index].GetType().Name;
-        shapes.RemoveAt(index);
-        Console.WriteLine($"Removed the {nameOfRemovedFigure} at index {index}");
+        else
+        {
+            Console.WriteLine("You entered an invalid index. Try again.");
+        }
     }
 
-    switch (command.ToLower())
+    switch (command)
     {
         case "circle":
-            double radius = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Please enter the radius of the circle:");
-                string input = Console.ReadLine();
-
-                if (double.TryParse(input, out radius))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("The input you entered is not valid!");
-                }
-            }
-
-            Circle circle = new Circle(radius);
-            shapes.Add(circle);
+            double radius = TryParseDouble("Please enter the radius of the circle:");
+            shapes.Add(new Circle(radius));
             Console.WriteLine("Circle created!");
             break;
 
         case "rectangle":
-            double sideA = 0;
-            double sideB = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Please enter side A of rectangle:");
-                string inputA = Console.ReadLine();
-                bool isValidA = false;
-                bool isValidB = false;
-
-                if (double.TryParse(inputA, out sideA))
-                {
-                    isValidA = true;
-                }
-
-                Console.WriteLine("Please enter side B of rectangle:");
-                string inputB = Console.ReadLine();
-
-                if (double.TryParse(inputB, out sideB))
-                {
-                    isValidB = true;
-                }
-
-                if (isValidA && isValidB)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("The input you entered is not valid!");
-                }
-            }
-
-            Rectangle rectangle = new Rectangle(sideA, sideB);
-            shapes.Add(rectangle);
+            double sideA = TryParseDouble("Please enter side A of the rectangle:");
+            double sideB = TryParseDouble("Please enter side B of the rectangle:");
+            shapes.Add(new Rectangle(sideA, sideB));
             Console.WriteLine("Rectangle created!");
             break;
 
         case "rhombus":
-            double firstDiagonal = 0;
-            double secondDiagonal = 0;
-            double side = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Please enter first rhombus diagonal:");
-                string first = Console.ReadLine();
-                bool isValidFirst = false;
-                bool isValidSecond = false;
-                bool isValidSide = false;
-
-                if (double.TryParse(first, out firstDiagonal))
-                {
-                    isValidFirst = true;
-                }
-
-                Console.WriteLine("Please enter second rhombus diagonal:");
-                string second = Console.ReadLine();
-
-                if (double.TryParse(second, out secondDiagonal))
-                {
-                    isValidSecond = true;
-                }
-
-                Console.WriteLine("Please enter rhombus side:");
-                string sideTemp = Console.ReadLine();
-
-                if (double.TryParse(sideTemp, out side))
-                {
-                    isValidSide = true;
-                }
-
-                if (isValidFirst && isValidSecond && isValidSide)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("The input you entered is not valid!");
-                }
-            }
-
-            Rhombus rhombus = new Rhombus(firstDiagonal, secondDiagonal, side);
-            shapes.Add(rhombus);
+            double firstDiagonal = TryParseDouble("Please enter the first diagonal of the rhombus:");
+            double secondDiagonal = TryParseDouble("Please enter the second diagonal of the rhombus:");
+            double side = TryParseDouble("Please enter the side of the rhombus:");
+            shapes.Add(new Rhombus(firstDiagonal, secondDiagonal, side));
             Console.WriteLine("Rhombus created!");
             break;
 
         case "square":
-            double squareSide = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Please enter the side of the square:");
-                string input = Console.ReadLine();
-
-                if (double.TryParse(input, out squareSide))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("The input you entered is not valid!");
-                }
-            }
-
-            Square square = new Square(squareSide);
-            shapes.Add(square);
+            double squareSide = TryParseDouble("Please enter the side of the square:");
+            shapes.Add(new Square(squareSide));
             Console.WriteLine("Square created!");
             break;
 
         case "triangle":
-            double @base = 0;
-            double height = 0;
-            double leftSide = 0;
-            double rightSide = 0;
-
-            while (true)
-            {
-                Console.WriteLine("Please enter the triangle base:");
-                string baseTemp = Console.ReadLine();
-                bool isValidBase = false;
-                bool isValidHeight = false;
-                bool isValidLeftSide = false;
-                bool isValidRightSide = false;
-
-                if (double.TryParse(baseTemp, out @base))
-                {
-                    isValidBase = true;
-                }
-
-                Console.WriteLine("Please enter the triangle height:");
-                string heightTemp = Console.ReadLine();
-
-                if (double.TryParse(heightTemp, out height))
-                {
-                    isValidHeight = true;
-                }
-
-                Console.WriteLine("Please enter triangle left side:");
-                string leftSideTemp = Console.ReadLine();
-
-                if (double.TryParse(leftSideTemp, out leftSide))
-                {
-                    isValidLeftSide = true;
-                }
-
-                Console.WriteLine("Please enter triangle right side:");
-                string rightSideTemp = Console.ReadLine();
-
-                if (double.TryParse(rightSideTemp, out rightSide))
-                {
-                    isValidRightSide = true;
-                }
-
-                if (isValidBase && isValidHeight && isValidLeftSide && isValidRightSide)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("The input you entered is not valid!");
-                }
-            }
-
-            Triangle triangle = new Triangle(@base, height, leftSide, rightSide);
-            shapes.Add(triangle);
+            double @base = TryParseDouble("Please enter the triangle base:");
+            double height = TryParseDouble("Please enter the triangle height:");
+            double leftSide = TryParseDouble("Please enter the left side of the triangle:");
+            double rightSide = TryParseDouble("Please enter the right side of the triangle:");
+            shapes.Add(new Triangle(@base, height, leftSide, rightSide));
             Console.WriteLine("Triangle created!");
             break;
 
         default:
+            Console.WriteLine("Invalid command. Try again.");
             break;
     }
 }
@@ -258,4 +94,36 @@ foreach (var shape in shapes)
     Console.WriteLine($"The area of the {shape.GetType().Name} is {shape.GetArea():F2}");
     Console.WriteLine($"The perimeter of the {shape.GetType().Name} is {shape.GetPerimeter():F2}");
     Console.WriteLine();
+}
+
+void SaveShapes(List<Shape> shapes)
+{
+    string json = JsonSerializer.Serialize(shapes, new JsonSerializerOptions { WriteIndented = true });
+    File.WriteAllText("shapes.json", json);
+    Console.WriteLine("Shapes saved to 'shapes.json'.");
+}
+
+List<Shape> LoadShapes()
+{
+    if (File.Exists("shapes.json"))
+    {
+        string json = File.ReadAllText("shapes.json");
+        return JsonSerializer.Deserialize<List<Shape>>(json) ?? new List<Shape>();
+    }
+    return new List<Shape>();
+}
+
+double TryParseDouble(string message)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+
+        if (double.TryParse(Console.ReadLine(), out double result))
+        {
+            return result;
+        }
+
+        Console.WriteLine("Invalid input. Please try again.");
+    }
 }
